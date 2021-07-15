dc29f2f63fcd5c121cbef8f236f920a0b7de6be9
Raul Elguezabal <jre0310@gmail.com>
2021-07-15 15:19:43 -0500

    pairs of four variables

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ def main():
     print(multiplication(x,y))
     # print(power(x,y))
     print(recMult(x,y))
-
+    fourVarPairs(10)
 
 
 def multiplication(x, y):
@@ -30,6 +30,29 @@ def recMult(x, y):
         return x
     else:
         return x + recMult(x, y - 1)
+
+def fourVarPairs(x):
+    n = x
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    while a < n:
+        while b < n:
+            while c < n:
+                while d < n:
+                    if a**2 + b == c**2 + d:
+                        print(a,b,c,d) 
+                    d += 1 
+                c += 1
+                d = 0 
+            b += 1
+            c = 0
+            d = 0 
+        a += 1
+        b = 0 
+        c = 0
+        d = 0
     
 
 
