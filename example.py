209c9b53d774399e64b0c16931f085e441837730
Jesus Elguezabal <jelguez@siue.edu>
2021-07-14 22:01:01 -0500

    multiplication functions

--- example.py
+++ example.py
@@ -1,1 +1,29 @@
-print("hello world")
+def main():
+    x = 4
+    y = 2
+    print(multiplication(x,y))
+    print("\n")
+    print(recMult(x,y))
+    
+
+
+
+def multiplication(x, y):
+    count = 0
+    num = 0
+    while count < y:
+        num = num + x
+        count += 1
+    return num
+
+def recMult(x , y):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return y
+    else:
+        return x + recMult(x, y - 1)
+    
+
+
+main()
