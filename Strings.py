3894612b96cc000bcd19cd5fdad8eacfb1c1a7dc
Jesus Elguezabal <jelguez@siue.edu>
2021-10-15 14:28:39 -0500

    worked through a few more examples and practice problems

--- Strings.py
+++ Strings.py
@@ -1,9 +1,16 @@
+import os
+
 def main():
-    # print(isUniqueChar(""))
+    # print(isUniqueChar("abc"))
     # printUniqueChar("the quick brown fox jumped over the lazy sheep dog")
     # print(checkPerm("jiujitsu ", "us tijuij"))
-    # reverseString("hello")
-    replaceChars("this is a string that I want to change", ' ', "%20")
+    # reverseString("elguezabal")
+    # replaceChars("this is a string that I want to change", 'a', "Fizz")
+    # print("Hello, " + os.getlogin())
+    fizzBuzz()
+
+""" An algorithm that determines if all characters in a string are unique.
+     returns true or false"""
 
 def isUniqueChar(string):
     count = 0
@@ -32,6 +39,7 @@ def printUniqueChar(string):
             count = 0
     print(charList)
 
+""" Check to see if string a is a permutation of string b """
 def checkPerm(a , b):
     if len(a) != len(b):
         return False
@@ -51,24 +59,69 @@ def checkPerm(a , b):
     else:
         return False 
 
+""" Reverse a given string """
 def reverseString(string):
-    stringList = []
+    # stringList = []
+    # for x in string:
+    #     stringList.append(x)
+    
+    # reversedStringList = []
+    # counter = len(string) - 1
+    # while counter >= 0:
+    #     reversedStringList.append(stringList[counter])
+    #     counter -= 1
+    
+    # reversedString = ''.join(reversedStringList)
+
+    str = ""
     for x in string:
-        stringList.append(x)
-    
-    reversedStringList = []
-    counter = len(string) - 1
-    while counter >= 0:
-        reversedStringList.append(stringList[counter])
-        counter -= 1
-    
-    reversedString = ''.join(reversedStringList)
-    print(reversedString)
+        str = x + str
+    print(str)
 
+" Replace a chosen character with a given character"
 def replaceChars(string, replaceChar, replaceWith):
-    replacedString = string.replace(replaceChar, replaceWith)
-    print(replacedString)
 
+    # replacedString = string.replace(replaceChar, replaceWith)
+    # print(replacedString)
+
+    replaceCharLoc = []
+
+    # Starting at index 0 find all the indicies where the replace char is in original string
+    index = 0
+    for x in string:
+        if x == replaceChar:
+            replaceCharLoc.append(index)
+        index += 1
+    
+    # Turn string into a list (cause strings are immutable)
+    newString = list(string)
+
+    # for every index in the replace array, go in at that index in the new string and replace 
+    # it with the replace char
+    for x in replaceCharLoc:
+        newString[x] = replaceWith
+    
+    # join the string converted to a list back into a string
+    newString = "".join(newString)
+
+    print(newString)
+
+'''Trying the classic fizz buzz problem'''
+def fizzBuzz():
+
+    for x in range(100):
+        # have to do this or else will print 0 - 99
+        num = x + 1
+
+        # remember you have to do check for 3 and 5 first or else it will get skipped afterwards
+        if (((num % 3) == 0) and ((num % 5) == 0)):
+            print("FizzBuzz")
+        elif ((num % 3) == 0):
+            print("Fizz")
+        elif ((num % 5) == 0):
+            print("Buzz")
+        else:
+            print(num)
 
     
 
